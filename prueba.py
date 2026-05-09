22a98b55f2111837b6198ed765d40f4e1773162b
derekvargaslopez27-byte <derekvargaslopez27@gmail.com>
2026-05-09 11:29:36 -0600

    prueba4.py

--- prueba.py
+++ prueba.py
@@ -1,3 +1,4 @@
-print("Aprediendo GitHub")
+a = int(input("Dijite el primer valor a sumar: "))
+b = int(input("Dijite el segundo valor a sumar: "))
 
-print("Como estas?")
+print(f"El resultado es {a + b} en total")
