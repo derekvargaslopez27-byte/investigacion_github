d7f9f1ad9590fd4554417822e801db69e566955f
derekvargaslopez27-byte <derekvargaslopez27@gmail.com>
2026-05-09 11:18:19 -0600

    tercer commit

---
+++ prueba2.py
@@ -0,0 +1,9 @@
+def dormir(dormir = False):
+    return dormir
+
+isJoelSnoosy = True
+
+if isJoelSnoosy:
+    print(f"Joel esta dormido? {dormir(True)}")
+else:
+    print(f"Joel esta dormido? {dormir(False)}")
